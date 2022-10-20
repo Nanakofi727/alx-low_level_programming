2c4e78f4618203c8fccf1d9ef6ce78c58c1ce791
Nanakofi727 <Nanakofi727@gmail.com>
2022-10-20 06:55:35 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0-9
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; x++)
+		_putchar(x + '0');
+	_putchar('\n');
+
+	return (0);
+}
