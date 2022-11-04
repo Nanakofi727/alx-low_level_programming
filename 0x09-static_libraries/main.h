d069221a2e65870818745afa5e54e06f229f92e7
Nanakofi727 <Nanakofi727@gmail.com>
2022-11-04 11:33:00 -0700

    task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 #include <stddef.h>
 
+/**
+ * file: main.h
+ * Auth: Derrick
+ * Desc: Header file containing all prototypes
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +28,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
